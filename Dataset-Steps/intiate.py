1ce194041f6c98fbb76af34f9976226127652c46
Shubham Gautam <57455438+shubhamgautam1211@users.noreply.github.com>
2022-06-19 23:31:27 +0530

    Update intiate.py

--- Dataset-Steps/intiate.py
+++ Dataset-Steps/intiate.py
@@ -1,3 +1,2 @@
 print("This file contains the steps and instruction that is to be performed on the datasets respectively")
 print("Algerbra and Statistical Operations is to be done using NumPy")
-print("Algerbra and Statistical Operations is to be done using NumPy")
